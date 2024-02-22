ad63b9b89a1b9cb29e61e97060f57be492ecb6df
Rinku <merinkukumar69@gmail.com>
2024-02-22 22:34:30 +0530

    bloc setup

--- lib/features/chat/bloc/chat_bloc.dart
+++ lib/features/chat/bloc/chat_bloc.dart
@@ -8,8 +8,11 @@ part 'chat_state.dart';
 
 class ChatBloc extends Bloc<ChatEvent, ChatState> {
   ChatBloc() : super(ChatInitial()) {
-    on<ChatEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<ChatNewPromptEvent>(chatNewPromptEvent);
+  }
+
+
+  FutureOr<void> chatNewPromptEvent(ChatNewPromptEvent event, Emitter<ChatState> emit) {
+
   }
 }
--- lib/features/chat/bloc/chat_event.dart
+++ lib/features/chat/bloc/chat_event.dart
@@ -2,3 +2,9 @@ part of 'chat_bloc.dart';
 
 @immutable
 abstract class ChatEvent {}
+
+class ChatNewPromptEvent extends ChatEvent {
+  final String prompt;
+
+  ChatNewPromptEvent({required this.prompt});
+}
